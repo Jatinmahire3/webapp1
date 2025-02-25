63f7ffe24c9b792143ab5be69f82ba5ec757996b
Jatinmahire3 <jatin.mahire24@vit.edu>
2025-02-25 16:42:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1118,4 +1118,5 @@ def ai_suggestion():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+      port = int(os.environ.get("PORT",4000))
+    app.run(host="0.0.0.0", post=port)
